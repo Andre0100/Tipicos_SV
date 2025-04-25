a360a2b3216d16fc29e6c2740dc59da96da59a44
Fabriciot1000@github.com <MT20005@ues.edu.sv>
2025-04-25 06:34:35 -0600

    prueba para jenkins

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/boundary/rest/server/BaseIntegrationAbstract.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/boundary/rest/server/BaseIntegrationAbstract.java
@@ -59,13 +59,13 @@ public abstract class BaseIntegrationAbstract {
         }
             
         Map<String, Object> propiedades = new HashMap<>();
-        propiedades.put("jakarta.persistence.jdbc.url", String.format("jdbc:postgresql://10.0.4.109:%d/PupaSV", postgres.getMappedPort(5432)));
+        propiedades.put("jakarta.persistence.jdbc.url", String.format("jdbc:postgresql://localhost:%d/PupaSV", postgres.getMappedPort(5432)));
         emf = Persistence.createEntityManagerFactory("PupaIP", propiedades);
     }
     
     protected String getBaseUrl() {
         if (this.getClass().isAnnotationPresent(NeedsLiberty.class)) {
-            return String.format("http://10.0.4.109:%d/PupaSV-1.0-SNAPSHOT/v1/", 
+            return String.format("http://localhost:%d/PupaSV-1.0-SNAPSHOT/v1/", 
                    ContainerExtension.getOpenLiberty().getMappedPort(9080));
         }
         return null;
