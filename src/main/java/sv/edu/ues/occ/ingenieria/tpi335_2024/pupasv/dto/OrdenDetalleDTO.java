ada4294f09c1d5ae3af253cbb0e978301799da20
Andrea <sorianolinares26@gmail.com>
2025-04-29 15:13:52 -0600

    Cobertura

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/dto/OrdenDetalleDTO.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/dto/OrdenDetalleDTO.java
@@ -11,8 +11,45 @@ import java.math.BigDecimal;
  * @author morales
  */
 public class OrdenDetalleDTO {
-    private Long idOrden; 
+
+    private Long idOrden;
     private Long idProductoPrecio;
     private Integer cantidad;
     private BigDecimal precio;
+
+    public OrdenDetalleDTO() {
+    }
+
+    public Long getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(Long idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public Long getIdProductoPrecio() {
+        return idProductoPrecio;
+    }
+
+    public void setIdProductoPrecio(Long idProductoPrecio) {
+        this.idProductoPrecio = idProductoPrecio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
 }
--- src/test/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/dto/OrdenDetalleDTOTest.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi335_2024/pupasv/dto/OrdenDetalleDTOTest.java
@@ -4,10 +4,43 @@
  */
 package sv.edu.ues.occ.ingenieria.tpi335_2024.pupasv.dto;
 
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+
 /**
  *
  * @author andrea
  */
 public class OrdenDetalleDTOTest {
+    
+     @Test
+    void testSettersAndGetters() {
+        System.out.println("OrdenDetalleDTOTest --> testSettersAndGetters");
+
+        OrdenDetalleDTO dto = new OrdenDetalleDTO();
+
+        dto.setIdOrden(1L);
+        dto.setIdProductoPrecio(10L);
+        dto.setCantidad(3);
+        dto.setPrecio(BigDecimal.valueOf(4.50));
+
+        assertEquals(1L, dto.getIdOrden());
+        assertEquals(10L, dto.getIdProductoPrecio());
+        assertEquals(3, dto.getCantidad());
+        assertEquals(BigDecimal.valueOf(4.50), dto.getPrecio());
+    }
+
+    @Test
+    void testConstructorVacio() {
+        System.out.println("OrdenDetalleDTOTest --> testConstructorVacio");
+
+        OrdenDetalleDTO dto = new OrdenDetalleDTO();
+        assertNull(dto.getIdOrden());
+        assertNull(dto.getIdProductoPrecio());
+        assertNull(dto.getCantidad());
+        assertNull(dto.getPrecio());
+    }
     
 }
